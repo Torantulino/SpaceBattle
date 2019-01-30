21be36dbe0389e2db3fe76c564ed091bd413155f
Torantulino <40276179@live.napier.ac.uk>
2019-01-30 21:25:22 +0000

    Added buildmode toggle to PlayerController.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -24,6 +24,7 @@ public partial class PlayerController : NetworkBehaviour {
     /// </summary>
     public Ship Ship { get; private set; }
 
+    private BuildController buildController;
     #endregion
 
     // Use this for initialization
@@ -39,23 +40,31 @@ public partial class PlayerController : NetworkBehaviour {
         // Check if this code runs on the game object that represents my Player
 	    if (!isLocalPlayer)
 	        return;
-        
-	    //todo temporary code for testing - remove later
-	    Ship.Thrust(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
-	    
-        //todo how to shoot - remove later
-        if (Input.GetKeyDown(KeyCode.Space))
-	        Ship.Shoot();
 
-        //todo changing aiming direction - upgrade later
-	    if (Input.GetKey(KeyCode.E))
-	        Ship.Target = Ship.Target + new Vector3(0f, Time.fixedDeltaTime * 30f, 0f);
-	    if (Input.GetKey(KeyCode.Q))
-	        Ship.Target = Ship.Target + new Vector3(0f, -Time.fixedDeltaTime * 30f, 0f);
+        //Flight & Fight Mode
+        if (!buildController.buildmode) {
+            //todo temporary code for testing - remove later
+            Ship.Thrust(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
 
-        //todo manually refreshing Ship's structure
-	    if (Input.GetKeyUp(KeyCode.R))
-            Ship.RefreshParts();
+            //todo how to shoot - remove later
+            if (Input.GetKeyDown(KeyCode.Space))
+                Ship.Shoot();
+
+            //todo changing aiming direction - upgrade later
+            if (Input.GetKey(KeyCode.E))
+                Ship.Target = Ship.Target + new Vector3(0f, Time.fixedDeltaTime * 30f, 0f);
+            if (Input.GetKey(KeyCode.Q))
+                Ship.Target = Ship.Target + new Vector3(0f, -Time.fixedDeltaTime * 30f, 0f);
+
+            //todo manually refreshing Ship's structure
+            if (Input.GetKeyUp(KeyCode.R))
+                Ship.RefreshParts();
+        }
+	    //Toggle build mode
+	    if (Input.GetKeyDown(KeyCode.Tab))
+	    {
+	        buildController.ToggleBuildmode();
+	    }
     }
 
 }
