1a246b1bfb735598e6d2f1dd962d9333ae1ef559
Torantulino <40276179@live.napier.ac.uk>
2019-01-30 20:35:42 +0000

    Added buildcontroller script.

---
+++ Assets/Scripts/Controllers/BuildController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Class for handling player input in buildmode
+/// </summary>
+public class BuildController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
