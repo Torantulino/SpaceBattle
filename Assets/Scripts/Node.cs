21acb3b2109125d1ac74daa50e4a416471b01263
Torantulino <40276179@live.napier.ac.uk>
2019-01-30 21:28:12 +0000

    Added Node script.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class representing building nodes
+/// </summary>
+
+public class Node : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
