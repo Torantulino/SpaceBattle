70b4cd5d8c0c5e3620ab52537db496016da85656
Daniel Korsah <dkorsah15@aol.co.uk>
2019-01-31 05:11:39 +0000

    Consolidate action in GUI facade. Switch build mode gui on/off with tab.

---
+++ Assets/Scripts/UI/GUIFacade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIFacade : MonoBehaviour
+{
+
+	//This class is used as a hub for calls coming from or to GUI processes/objects
+
+	//build mode panel
+	public static GUIToggle BuildPanel;
+
+	void Start()
+	{
+		BuildPanel = GameObject.Find("BuildPanel").GetComponent<GUIToggle>();
+	}
+
+	public void ToggleBuildMode()
+	{
+		ToggleElement(BuildPanel);
+	}
+
+	/// <summary>
+	/// Calls the toggle script on an object which has been defined on the object calling this function in the Unity Editor. 
+	///<para>Allows Toggle method to change in future.</para>
+	/// </summary>
+	/// <param name="toggle">Takes only objects with GUIToggle scripts attached.</param>
+	public void ToggleElement(GUIToggle toggle)
+	{
+		toggle.Flip();
+	}
+}
---
+++ Assets/Scripts/UI/KeyboardListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardListener : MonoBehaviour
+{
+
+	GUIFacade gui;
+
+	void Start()
+	{
+		gui = GameObject.Find("GUI_Interface").GetComponent<GUIFacade>();
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown("tab"))
+		{
+			gui.ToggleBuildMode();
+		}
+	}
+}
