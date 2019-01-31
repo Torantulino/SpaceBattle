07d68634ccafb002f68a9893d419b638bbc0b6ee
Daniel Korsah <dkorsah15@aol.co.uk>
2019-01-31 02:14:18 +0000

    Script made and attached to toggle the inventory window active/inactive.

---
+++ Assets/Scripts/UI/GUIToggle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIToggle : MonoBehaviour
+{
+
+	public void Flip()
+	{
+		gameObject.SetActive(!gameObject.activeSelf);
+	}
+}
