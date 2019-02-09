304ef004423022f56b898f2f6c007d078c92a294
Daniel Korsah <dkorsah15@aol.co.uk>
2019-02-09 13:45:10 +0000

    Combat mode Camera and a way of testing the  following works.

---
+++ Assets/Scripts/Misc/AutoMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+	public float speed;
+	public float rotation;
+	// Update is called once per frame
+	void Update()
+	{
+
+		float mod = Mathf.Sin(Time.time);
+
+		gameObject.transform.position += new Vector3(mod * speed, 0, 0) * Time.deltaTime;
+
+		Vector3 rot = new Vector3(0, 0, mod * 90);
+		gameObject.transform.rotation = Quaternion.Euler(rot);
+
+	}
+}
