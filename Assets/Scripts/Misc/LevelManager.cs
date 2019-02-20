232b3d99ca39e90e229008e4f4a0b6ed32e032de
Torantulino <40276179@live.napier.ac.uk>
2019-02-20 02:46:16 +0000

    Added rudementary asteroid field generation.

---
+++ Assets/Scripts/Misc/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 500; i++)
+        {
+            GameObject asteroid = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            asteroid.transform.position = Random.insideUnitSphere * 1000.0f;
+            asteroid.transform.localScale = Vector3.one * Random.Range(5.0f, 40.0f);
+            asteroid.transform.rotation = Random.rotation;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
