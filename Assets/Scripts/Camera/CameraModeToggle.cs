da912ddbd0997f90564fcc31a0856ff84d51edf3
Torantulino <40276179@live.napier.ac.uk>
2019-02-20 00:22:09 +0000

    Added method to calculate the bounds of the ship with all it's parts.

--- Assets/Scripts/Camera/CameraModeToggle.cs
+++ Assets/Scripts/Camera/CameraModeToggle.cs
@@ -26,8 +26,8 @@ public class CameraModeToggle : MonoBehaviour
 	// Use lateupdate to try to act after any Cinemachine scripts to avoid jitter (not really working)
 	void LateUpdate()
 	{
-		//if a player with a camera anchor exists:
-		if (CameraController.CameraAnchor != null)
+        //if a player with a camera anchor exists:
+        if (CameraController.CameraAnchor != null)
 		{
 			//if in build mode don't invert mouse and more focus point ot centre on ship
 			if (buildMode)
@@ -67,18 +67,11 @@ public class CameraModeToggle : MonoBehaviour
                 flightCam.gameObject.SetActive(true);
                 buildCam.gameObject.SetActive(false);
 
+                //Move lookat anchor infront of ship
                 CameraController.CameraAnchor.transform.localPosition = new Vector3(0.0f, 0.0f, 10.0f);
-				//cam.m_YAxis.m_InvertAxis = true;
-				//cam.m_Orbits[1].m_Radius = 1.08f;
 
-				////set centre radius
-				//cam.m_Orbits[1].m_Radius = combatRadius;
-
-				////set top and bottom heights
-				//cam.m_Orbits[0].m_Height = combatHeight;
-				//cam.m_Orbits[2].m_Height = -combatHeight;
-
-				if (Input.GetKey(KeyCode.LeftShift))
+                //Release mouse - Testing only?
+                if (Input.GetKey(KeyCode.LeftShift))
 				{
 					Cursor.lockState = CursorLockMode.None;
 					Cursor.visible = true;
@@ -99,6 +92,27 @@ public class CameraModeToggle : MonoBehaviour
     public void UpdateBuildmode(bool b)
     {
         buildMode = b;  //Hard set rather than toggle to ensure sync
+    }
+
+    //Calculates the total bounds for the player and it's children. This includes the 'ghost' of unbuilt parts (as you want to see them) but not the visualiser.
+    private float CalculateBounds()
+    {
+        float furthest = 0.0f;
+
+        //Obtain the furthest out value
+        foreach (Renderer childRend in GameController.LocalPlayer.GetComponentsInChildren<Renderer>()) {
+            if (childRend.name != "Visualiser") { 
+                Vector3 min = childRend.bounds.min - GameController.LocalPlayer.transform.position;
+                Vector3 max = childRend.bounds.max - GameController.LocalPlayer.transform.position;
+
+                if (min.magnitude > furthest)
+                    furthest = min.magnitude;
+
+                if (max.magnitude > furthest)
+                    furthest = max.magnitude;
+            }
+        }
+        return furthest;
     }
 
 }
