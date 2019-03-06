a0ba34982860f3e86bcc036e10bbfcecb7a2b0bc
Daniel Korsah <dkorsah15@aol.co.uk>
2019-03-06 02:09:16 +0000

    Experiment with different style of orientation.

---
+++ Assets/Scripts/UI/PlayerIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIndicator : MonoBehaviour
+{
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (GameController.LocalPlayer)
+		{
+			gameObject.transform.localRotation = GameController.LocalPlayer.transform.rotation;
+		}
+	}
+}
