8672baf887bebf366b1949d9c312408af8e3bf0c
Krzysztof Czerwinski <substrifer@gmail.com>
2019-03-13 19:42:37 +0000

    Added Engine particles

---
+++ Assets/Scripts/Extensions/ParticleSystemExtension.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public static class ParticleSystemExtension
+{
+    public static void EnableEmission(this ParticleSystem particleSystem, bool enabled)
+    {
+        var emission = particleSystem.emission;
+        emission.enabled = enabled;
+    }
+
+    public static float GetEmissionRate(this ParticleSystem particleSystem)
+    {
+        return particleSystem.emission.rateOverTime.constantMax;
+    }
+
+    public static void SetEmissionRate(this ParticleSystem particleSystem, float emissionRate)
+    {
+        var emission = particleSystem.emission;
+        var rate = emission.rateOverTime;
+        rate.constantMax = emissionRate;
+        emission.rateOverTime = rate;
+    }
+}
