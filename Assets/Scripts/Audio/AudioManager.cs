551e41106fedbbae21ccb2630ede96d0a468931c
Korsah <40214314@live.napier.ac.uk>
2019-04-17 14:19:33 +0100

    All FMOD stuff.

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public List<string> itemEventStrings;
+    public List<FMOD.Studio.EventInstance> audioEvents;
+
+    // Use this for initialization
+    void Start () {
+
+    audioEvents = new List<FMOD.Studio.EventInstance> {
+            { FMODUnity.RuntimeManager.CreateInstance(itemEventStrings[0]) },
+    };
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
